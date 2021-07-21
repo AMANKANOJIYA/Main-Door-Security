edb568d42672cd71c55a832b42a56cde1ebce946
Aman kanojiya <50018596+AMANKANOJIYA@users.noreply.github.com>
2021-07-21 08:41:49 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -6,9 +6,6 @@ API_KEY = '5e3749d7-73f0-4c2a-bb6c-dda24d7c6a07'
 DEVICE_ID = 'BOLT5780232'
 USERNAME="1"
 PASSWORD="1"
-
-"""Configurations for telegram_alert.py"""
-BOLT_API_KEY = "5e3749d7-73f0-4c2a-bb6c-dda24d7c6a07"                 # This is your Bolt Cloud API Key
-DEVICE_ID = "BOLT5780232"                    # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
-TELEGRAM_CHAT_ID = "@main_door_security"            # This is the channel ID of the created Telegram channel. Paste after @ symbol.
-TELEGRAM_BOT_ID = "bot1918791309:AAEdE09ryAjhoSF1kVlTjmGAMYHdekTL_Sc"           # This is the bot ID of the created Telegram Bot. Paste after bot text.
+BOLT_API_KEY = "5e3749d7-73f0-4c2a-bb6c-dda24d7c6a07"
+TELEGRAM_CHAT_ID = "@main_door_security"
+TELEGRAM_BOT_ID = "bot1918791309:AAEdE09ryAjhoSF1kVlTjmGAMYHdekTL_Sc"
